685b15e8777455775bd0088ab4204c2d062fa017
Rio Chandra Rajagukguk <riochr17@gmail.com>
2020-01-17 13:18:09 +0700

    Updates

---
+++ precache-manifest.689885eb493e367d0fb3556a4ab1e72c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c437019d70031efc27446dbd03ff31a0",
+    "url": "/random-test/index.html"
+  },
+  {
+    "revision": "5b0bc25992d079908969",
+    "url": "/random-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b44503b7e963b6aafc70",
+    "url": "/random-test/static/js/2.b9706e43.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/random-test/static/js/2.b9706e43.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5b0bc25992d079908969",
+    "url": "/random-test/static/js/main.0b8d531b.chunk.js"
+  },
+  {
+    "revision": "4c32efe83f723f9a06da",
+    "url": "/random-test/static/js/runtime-main.a29d63df.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/random-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
